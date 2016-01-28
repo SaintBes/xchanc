1143d09d473d8611701035570d93731709b48b4c
sergey_shapovalenko <ssa.root@gmail.com>
2016-01-28 17:18:16 +0200

    upd 27

--- src/main/java/chanc/controller/IndexController.java
+++ src/main/java/chanc/controller/IndexController.java
@@ -4,10 +4,17 @@ package chanc.controller;
 
 import org.apache.log4j.Logger;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 @Controller
@@ -16,11 +23,45 @@ import javax.servlet.http.HttpSession;
 public class IndexController {
     public static final Logger log = Logger.getLogger(IndexController.class);
 
+    @Autowired
+    UserDetailsService userDetailsService;
+
     @RequestMapping(value = "/", method = {RequestMethod.GET, RequestMethod.HEAD, RequestMethod.POST})
     public String index(HttpServletRequest request, Model model, HttpSession session) {
         model.addAttribute("md1", "opa-opa");
 
 //        log.info("/index controller");
-        return "index";
+        return "login";
     }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String loginPage() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null) {
+            return "dashboard";
+        }
+
+        return "login";
+    }
+
+    @RequestMapping(value="/logout", method = RequestMethod.GET)
+    public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null){
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "redirect:/login?logout";
+    }
+
+//    private String getPrincipal(){
+//        String userName = null;
+//        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+//
+//        if (principal instanceof UserDetails) {
+//            userName = ((UserDetails)principal).getUsername();
+//        } else {
+//            userName = principal.toString();
+//        }
+//        return userName;
+//    }
 }
